7e86446dae01196242e313395441ec4f0788aaa5
balapavithra <pavikannan2991@gmail.com>
2023-07-29 11:54:56 +0530

    new

---
+++ task6.js
@@ -0,0 +1,5 @@
+// Merge two array elements and return them in ascending order.
+// Input: array1 = [21, 58, 69, 20, 47, 55, 7, 36, 91, 75];
+// array2 = [12, 34, 46, 22, 67, 89];
+// Output: 7, 12, 20, 21, 22, 34, 36, 46, 47, 55, 58, 67, 69, 75, 89, 91
+
